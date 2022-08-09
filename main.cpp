897ee0bc6c6ec5ef7683f3363d5510ce79bac85b
sami-theion <sami@theion.de>
2022-08-09 13:59:33 +0200

    fixed values

--- main.cpp
+++ main.cpp
@@ -14,7 +14,19 @@ int main()
     {
         std::cout << "error adress" << std::endl;
     }
-    RC rc = LKIF2_OpenDeviceETHER(&paramEther);
-    
+    std::cout << "open connection to " <<strIp << std::endl;
+    RC rceth = LKIF2_OpenDeviceETHER(&paramEther);
+	int outNo0 = 0;
+	LKIF_FLOATVALUE_OUT value;
+	RC rc0 = LKIF2_GetCalcDataSingle((outNo0),&value);
+    std::cout << "get value outputnr:" <<value.OutNo<<" Res: "<<value.FloatResult <<" Value:"<<value.Value<< std::endl;
+	int outNo1 = 1;
+	LKIF_FLOATVALUE_OUT value1;
+	RC rc1 = LKIF2_GetCalcDataSingle((outNo1),&value1);
+    std::cout << "get value  outputnr:" <<value1.OutNo<<" Res: " <<value1.FloatResult <<" Value:"<<value1.Value<< std::endl;
+	int outNo2 = 2;
+	LKIF_FLOATVALUE_OUT value2;
+	RC rc2 = LKIF2_GetCalcDataSingle((outNo2),&value2);
+    std::cout << "get value  outputnr:" <<value2.OutNo<<" Res: " <<value2.FloatResult <<" Value:"<<value2.Value<< std::endl;
     return 0;
 }
