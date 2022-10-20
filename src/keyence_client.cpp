979e705e1b04f8fac77a6e484c7a40aec770cc26
sami-theion <sami@theion.de>
2022-10-20 16:04:53 +0200

    updated lib cmake

--- src/keyence_client.cpp
+++ src/keyence_client.cpp
@@ -1,6 +1,6 @@
 #include "keyence_client.h"
 
-keyence_client::keyence_client(const char* ip ) :IP(ip) {}
+keyence_client::keyence_client(const char* ip) :IP(ip) {}
 enum_hw_feedback keyence_client::connect()
 {
 
@@ -14,25 +14,31 @@ enum_hw_feedback keyence_client::connect()
     std::cout << "open connection to " << IP << std::endl;
     RC Conn = LKIF2_OpenDeviceETHER(&paramEther);
     if (Conn == RC_OK)
-        std::cout << "connection success" << std::endl;
+    {
+        std::cout << "keyence connection success" << std::endl;
         return enum_hw_feedback::hw_success;
-    return enum_hw_feedback::hw_error;
+    }
+    else
+    {
+        std::cout << "keyence connection failed" << std::endl;
+        return enum_hw_feedback::hw_error;
+    }
 
 }
 double keyence_client::get_value_output(int outputNr)
 {
-        RC Val = LKIF2_GetCalcDataSingle((outputNr), &value);
-        if (DataIsValid())
-        {
-         std::cout << "get valid data from output number: " << outputNr << " Value:" << value.Value << std::endl;
+    RC Val = LKIF2_GetCalcDataSingle((outputNr), &value);
+    if (DataIsValid())
+    {
+        std::cout << "get valid data from output number: " << outputNr << " Value:" << value.Value << std::endl;
         return value.Value;
-        }
-        std::cout << "invalid datafrom output number: " << outputNr  << " Value:" << value.Value << std::endl;
-        return NULL;
+    }
+    std::cout << "invalid datafrom output number: " << outputNr << " Value:" << value.Value << std::endl;
+    return NULL;
 }
 bool keyence_client::DataIsValid()
 {
-     if (value.FloatResult == LKIF_FLOATRESULT_VALID) return true;
-     return false;
+    if (value.FloatResult == LKIF_FLOATRESULT_VALID) return true;
+    return false;
 
 }
