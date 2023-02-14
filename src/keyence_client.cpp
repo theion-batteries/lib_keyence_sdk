24180eb92154cf9e9b96be0e48de3bd62221ce94
sami-theion <sami@theion.de>
2023-02-14 19:30:40 +0100

    added whsController tests

--- src/keyence_client.cpp
+++ src/keyence_client.cpp
@@ -1,12 +1,14 @@
 #include "keyence_client.h"
 
-keyence_client::keyence_client(const char* ip) :IP(ip) {}
+keyence_client::keyence_client(std::string ip):IP(ip) {}
 wgm_feedbacks::enum_hw_feedback keyence_client::connect()
 {
 
     std::cout << "connecting to keyence keyence controller via tcp" << std::endl;
+    std::cout << "keyence server ip:  " << IP << std::endl;
+
     LKIF_OPENPARAM_ETHERNET paramEther;
-    paramEther.IPAddress.S_un.S_addr = inet_addr(IP);
+    paramEther.IPAddress.S_un.S_addr = inet_addr(IP.c_str());
     if (paramEther.IPAddress.S_un.S_addr == INADDR_NONE)
     {
         std::cout << "error IP" << std::endl;
@@ -28,13 +30,26 @@ wgm_feedbacks::enum_hw_feedback keyence_client::connect()
 }
 double keyence_client::get_value_output(int outputNr)
 {
-    RC Val = LKIF2_GetCalcDataSingle((outputNr), &value);
+    RC Val = LKIF2_GetCalcDataSingle(outputNr, &value);
     if (DataIsValid())
     {
         std::cout << "get valid data from output number: " << outputNr << " Value:" << value.Value << std::endl;
         return value.Value;
     }
     std::cout << "invalid datafrom output number: " << outputNr << " Value:" << value.Value << std::endl;
+    return NULL;
+}
+
+double keyence_client::get_value_all()
+{
+    int all[] = {0,1,2};
+    RC Val = LKIF2_GetCalcDataALL(all, &value);
+    if (DataIsValid())
+    {
+        std::cout << "get valid data from output number: " << all << " Value:" << value.Value << std::endl;
+        return value.Value;
+    }
+    std::cout << "invalid datafrom output number: " << all << " Value:" << value.Value << std::endl;
     return NULL;
 }
 bool keyence_client::DataIsValid()
